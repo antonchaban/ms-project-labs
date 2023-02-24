1801597558e5117da8da44c78a69a6b6fbce7c21
Anton Chaban <71810009+antonchaban@users.noreply.github.com>
2023-02-24 18:25:42 +0200

    initial commit

---
+++ src/main/java/ua/kpi/fict/chaban/msprojectlabs/MsProjectLabsApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.fict.chaban.msprojectlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProjectLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsProjectLabsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kpi/fict/chaban/msprojectlabs/MsProjectLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.fict.chaban.msprojectlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProjectLabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
